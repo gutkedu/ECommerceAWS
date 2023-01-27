b49bf280b36c60fc1fa91e35267a15e47d29ef86
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-01-26 22:16:19 -0300

    feat: adding the table read access to lambda func

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -36,7 +36,12 @@ export class ProductsAppStack extends cdk.Stack {
           minify: true,
           sourceMap: false,
         },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
       },
     );
+
+    this.productsDdb.grantReadData(this.productsFetchHandler);
   }
 }
