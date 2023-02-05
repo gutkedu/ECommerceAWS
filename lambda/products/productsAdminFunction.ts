0d870940a0d51537125c443c3c1aacf7ac577dfc
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-02-05 18:13:20 -0300

    feat: admin function with dynamo db

--- lambda/products/productsAdminFunction.ts
+++ lambda/products/productsAdminFunction.ts
@@ -3,6 +3,12 @@ import {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+import { Product, ProductRepository } from '/opt/nodejs/productsLayer';
+
+const productsDdb = process.env.PRODUCTS_DDB!;
+const ddbClient = new DynamoDB.DocumentClient();
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 export async function handler(
   event: APIGatewayProxyEvent,
@@ -18,25 +24,48 @@ export async function handler(
 
   if (event.resource === '/products') {
     console.log('POST /products');
+    const product = JSON.parse(event.body!) as Product;
+    const createdProduct = await productRepository.create(product);
     return {
       statusCode: 201,
-      body: JSON.stringify({ message: 'hello from post products' }),
+      body: JSON.stringify(createdProduct),
     };
   } else if (event.resource === '/products/{id}') {
     const productId = event.pathParameters!.id as string;
 
     if (method === 'PUT') {
       console.log(`PUT /products/{${productId}}`);
-      return {
-        statusCode: 200,
-        body: `hello from PUT /products/{${productId}}`,
-      };
+      const product = JSON.parse(event.body!) as Product;
+      try {
+        const updatedProduct = await productRepository.updateProduct(
+          productId,
+          product,
+        );
+        return {
+          statusCode: 200,
+          body: JSON.stringify(updatedProduct),
+        };
+      } catch (ConditionalCheckFailedException) {
+        return {
+          statusCode: 404,
+          body: `Product with id ${productId} does not exist`,
+        };
+      }
     } else if (method === 'DELETE') {
       console.log(`DELETE /products/{${productId}}`);
-      return {
-        statusCode: 200,
-        body: `hello from DELETE /products/{${productId}}`,
-      };
+      try {
+        const deletedProduct = await productRepository.deleteById(productId);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(deletedProduct),
+        };
+      } catch (error) {
+        console.error((<Error>error).message);
+        return {
+          statusCode: 404,
+          body: (<Error>error).message,
+        };
+      }
     }
   }
   return {
